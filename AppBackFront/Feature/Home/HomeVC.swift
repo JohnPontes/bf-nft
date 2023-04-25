4cc98c860fd337d2ee9f911ff6cb7f277d534741
John Pontes <jwesleysilvap@gmaill.com>
2023-04-25 00:01:32 -0300

    feat: create tab bar

---
+++ AppBackFront/Feature/Home/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  AppBackFront
+//
+//  Created by John Pontes on 24/04/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
