6e2fc21a69593387025b16b315131db2f1ae1a02
John Pontes <jwesleysilvap@gmaill.com>
2023-04-26 22:27:54 -0300

    feat: add elements and protocols in home screen

--- AppBackFront/Feature/Home/Screen/HomeScreen.swift
+++ AppBackFront/Feature/Home/Screen/HomeScreen.swift
@@ -11,8 +11,9 @@ class HomeScreen: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .white
+        backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
         addViews()
+        configConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -30,7 +31,7 @@ class HomeScreen: UIView {
     lazy var logoImageView: UIImageView = {
         let img = UIImageView()
         img.translatesAutoresizingMaskIntoConstraints = false
-        img.image = UIImage(named: "vector")
+        img.image = UIImage(named: "Vector")
         
         return img
     }()
@@ -46,7 +47,7 @@ class HomeScreen: UIView {
     }()
     
     lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
@@ -69,11 +70,54 @@ class HomeScreen: UIView {
         return tableView
     }()
     
+    func configTableViewProtocols(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
+    
+    func configCollectionViewProtocols(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
+        collectionView.delegate = delegate
+        collectionView.dataSource = dataSource
+    }
+    
+    func configSearchBarProtocols(delegate: UISearchBarDelegate) {
+        searchBar.delegate = delegate
+    }
+    
     private func addViews() {
         addSubview(viewBackGround)
         viewBackGround.addSubview(logoImageView)
         viewBackGround.addSubview(searchBar)
         viewBackGround.addSubview(collectionView)
         addSubview(tableView)
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            viewBackGround.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            viewBackGround.leadingAnchor.constraint(equalTo: leadingAnchor),
+            viewBackGround.trailingAnchor.constraint(equalTo: trailingAnchor),
+            viewBackGround.heightAnchor.constraint(equalToConstant: 220),
+            
+            logoImageView.topAnchor.constraint(equalTo: viewBackGround.topAnchor, constant: 8),
+            logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            logoImageView.widthAnchor.constraint(equalToConstant: 40),
+            logoImageView.heightAnchor.constraint(equalToConstant: 40),
+            
+            searchBar.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 10),
+            searchBar.leadingAnchor.constraint(equalTo: logoImageView.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            searchBar.heightAnchor.constraint(equalToConstant: 46),
+            
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: viewBackGround.bottomAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: viewBackGround.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
